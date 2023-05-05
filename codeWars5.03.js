38a16873755dd4e39cb7c853fe5b6cb8e23272b6
Garrett Pace <elderpace6@gmail.com>
2023-05-04 19:44:43 -0600

    1stTry

---
+++ codeWars5.03.js
@@ -0,0 +1,9 @@
+function makeNegative(num) {
+    return Math.abs(num) * -1
+  }
+  
+  console.log(makeNegative(-5))
+  console.log(makeNegative(5))
+  console.log(makeNegative(-2))
+  console.log(makeNegative(-1))
+  console.log(makeNegative(0))
